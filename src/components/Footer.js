263c9f0636d931c0ff20204bfd0b2baf816584a2
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-04-02 03:11:37 -0700

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <p>
+            © Copyright: Samaneh Vajdi
+            <a href="/about">About</a>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
