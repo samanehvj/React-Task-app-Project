c683c034bf2d5cfe108afebf62cb8dcb51303712
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-04-02 03:11:35 -0700

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h3>About US</h3>
+            <a href="/" HomePage>
+
+            </a>
+        </div>
+    )
+}
+
+export default About
